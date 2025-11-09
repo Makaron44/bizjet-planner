fbf50c625c1248164d0eb9e585479d3419f4c66b
Makaron44 <99068890+Makaron44@users.noreply.github.com>
2025-11-09 18:17:08 +0100

    pliki

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE = "bizjet-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+});
